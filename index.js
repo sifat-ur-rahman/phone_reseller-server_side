6cadcb36cbc475ab6086b12369fe1f1a2c39c685
sifat-ur-rahman <md.sifat.ur.rahman2702@gmail.com>
2022-11-25 19:15:49 +0600

    add

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sb5ycdx.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const productCollection = client.db('phone').collection('product')
+
+        // app.get('/category/:id', async(req, res) =>{
+            
+        //     const id = req.params.id;
+        //     const filter = { "category-id" : parseInt(id) }
+            
+        //     const result = await productCollection.find(filter).toArray()
+        //     res.send(result)
+        // })
+
+        app.get('/category/:category', async(req, res) =>{
+            
+            const categoryName = req.params.category;
+            const filter = { "category" : categoryName }
+            
+            const result = await productCollection.find(filter).toArray()
+            res.send(result)
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.log)
 
 
 app.get('/', async(req, res) =>{
